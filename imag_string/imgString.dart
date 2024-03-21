582f6426377d2593f3a8d5b2fae876c7e3392113
Kritikagupta20 <kritikagupta8377@gmail.com>
2024-03-21 13:48:20 +0530

    21march

---
+++ imag_string/imgString.dart
@@ -0,0 +1,8 @@
+const String jSplash = "assets/images/jewelery/logo/splash.png";
+const String jSplash1 = "assets/images/splash_img/circle.png";
+
+const String mWelcomeimg = "assets/images/welcome_img/girlphone1.png";
+const String mWelcomeimg2 = "assets/images/welcome_img/eatenoodles.png";
+
+const String mgoogle = "assets/images/welcome_img/google.png";
+const String mforgotimg = "assets/images/forgotpassword/forgotimg.png";

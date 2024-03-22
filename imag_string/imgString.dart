07151cc24c86591634ee17739544dab48cbe37be
Kritikagupta20 <kritikagupta8377@gmail.com>
2024-03-22 18:43:03 +0530

    changes done

--- imag_string/imgString.dart
+++ imag_string/imgString.dart
@@ -1,8 +1,51 @@
-const String jSplash = "assets/images/jewelery/logo/splash.png";
-const String jSplash1 = "assets/images/splash_img/circle.png";
+const String jSplash1 = "assets/images/jewelery/logo/splash1.png";
 
-const String mWelcomeimg = "assets/images/welcome_img/girlphone1.png";
-const String mWelcomeimg2 = "assets/images/welcome_img/eatenoodles.png";
+const String mstarting = "assets/images/jewelery/logo/starting.png";
+const String msignup = "assets/images/jewelery/signup.jpeg";
 
-const String mgoogle = "assets/images/welcome_img/google.png";
-const String mforgotimg = "assets/images/forgotpassword/forgotimg.png";
+const String mforgotimg = "assets/images/jewelery/logo/forgotimg.png";
+
+const String mearing1 = "assets/images/jewelery/earings/earing1.jpeg";
+const String mearing2 = "assets/images/jewelery/earings/earing2.jpeg";
+const String mearing3 = "assets/images/jewelery/earings/earing3.jpeg";
+const String mearing4 = "assets/images/jewelery/earings/earing4.jpeg";
+const String mearing5 = "assets/images/jewelery/earings/earing5.jpeg";
+
+const String mearingcartoon =
+    "assets/images/jewelery/earings/earingcartoon.png";
+
+const String mhands = "assets/images/jewelery/hands/hands1.jpeg";
+const String mhands1 = "assets/images/jewelery/hands/hands2.jpeg";
+const String mhands2 = "assets/images/jewelery/hands/hands3.jpeg";
+const String mhands3 = "assets/images/jewelery/hands/hands4.jpeg";
+const String mhands4 = "assets/images/jewelery/hands/hands5.jpeg";
+
+const String mhead = "assets/images/jewelery/head/head1.jpeg";
+const String mhead1 = "assets/images/jewelery/head/head2.jpeg";
+const String mhead2 = "assets/images/jewelery/head/head3.jpeg";
+const String mhead3 = "assets/images/jewelery/head/head4.jpeg";
+const String mhead4 = "assets/images/jewelery/head/head5.jpeg";
+
+const String mneck = "assets/images/jewelery/neck/neck1.jpeg";
+const String mneck1 = "assets/images/jewelery/neck/neck2.jpeg";
+const String mneck2 = "assets/images/jewelery/neck/neck3.jpeg";
+const String mneck3 = "assets/images/jewelery/neck/neck4.jpeg";
+const String mneck4 = "assets/images/jewelery/neck/neck5.jpeg";
+
+const String mpayal = "assets/images/jewelery/payal/payal1.jpeg";
+const String mpayal1 = "assets/images/jewelery/payal/payal2.jpeg";
+const String mpayal2 = "assets/images/jewelery/payal/payal3.jpeg";
+const String mpayal3 = "assets/images/jewelery/payal/payal4.jpeg";
+const String mpayal4 = "assets/images/jewelery/payal/payal5.jpeg";
+
+const String mring = "assets/images/jewelery/ring/ring1.jpeg";
+const String mring1 = "assets/images/jewelery/ring/ring2.jpeg";
+const String mring2 = "assets/images/jewelery/ring/ring3.jpeg";
+const String mring3 = "assets/images/jewelery/ring/ring4.jpeg";
+const String mring4 = "assets/images/jewelery/ring/ring5.jpeg";
+
+const String mset = "assets/images/jewelery/set/set1.jpeg";
+const String mset1 = "assets/images/jewelery/set/set2.jpeg";
+const String mset2 = "assets/images/jewelery/set/set3.jpeg";
+const String mset3 = "assets/images/jewelery/set/set4.jpeg";
+const String mset4 = "assets/images/jewelery/set/set5.jpeg";
